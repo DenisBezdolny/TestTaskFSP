edf8dd3e4716827add31b6b9ac34863b0363dd58
DenisBezdolny <d.i.d.k.o.d.a@gmail.com>
2025-04-12 17:23:07 +0300

    Added User Service

---
+++ TestTask/TestTask.Bll/Services/UserService.cs
@@ -0,0 +1,42 @@
+﻿using TestTask.Domain.Entities;
+using TestTask.Domain.Interfaces.Bll;
+using TestTask.Domain.Interfaces.Repositories;
+
+namespace TestTask.Bll.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IRepository<User> _userRepository;
+
+        public UserService(IRepository<User> userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public Task<IEnumerable<User>> GetAllAsync()
+        {
+            return _userRepository.GetAllAsync();
+        }
+
+        public Task<User?> GetByIdAsync(Guid id)
+        {
+            return _userRepository.GetByIdAsync(id);
+        }
+
+        public async Task CreateAsync(User user)
+        {
+            user.Id = Guid.NewGuid();
+            await _userRepository.CreateAsync(user);
+        }
+
+        public Task UpdateAsync(User user)
+        {
+            return _userRepository.UpdateAsync(user);
+        }
+
+        public Task DeleteAsync(Guid id)
+        {
+            return _userRepository.DeleteAsync(id);
+        }
+    }
+}
