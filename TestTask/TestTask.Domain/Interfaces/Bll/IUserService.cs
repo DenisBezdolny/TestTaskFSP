d82370276f4e74c9efe1b7b201056ce4d5c95e9e
DenisBezdolny <d.i.d.k.o.d.a@gmail.com>
2025-04-12 17:11:05 +0300

    Added Methods in interface

--- TestTask/TestTask.Domain/Interfaces/Bll/IUserService.cs
+++ TestTask/TestTask.Domain/Interfaces/Bll/IUserService.cs
@@ -1,6 +1,13 @@
-﻿namespace TestTask.Domain.Interfaces.Bll
+﻿using TestTask.Domain.Entities;
+
+namespace TestTask.Domain.Interfaces.Bll
 {
     public interface IUserService
     {
+        Task<IEnumerable<User>> GetAllAsync();
+        Task<User?> GetByIdAsync(Guid id);
+        Task CreateAsync(User user);
+        Task UpdateAsync(User user);
+        Task DeleteAsync(Guid id);
     }
 }
